a6b6602e81ed2307cbf9ce7609469da749706068
liha4ini <liha4ev.sergey@gmail.com>
2023-09-10 19:48:20 +0300

    add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 
 import './App.css'
+import {Button} from "./components/Button/Button";
 
 function App() {
 
   return (
     <>
         <h1>Hello!!!!!!!!!!!!!!</h1>
+        <Button />
     </>
   )
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import './Button.css'
+
+export const Button = () => {
+    return (
+        <div>
+            <button className='button accent'>Save</button>
+        </div>
+    );
+};
+
