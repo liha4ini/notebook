c0f0e37d04147371c990d92968a3b4af0d1044e7
liha4ini <liha4ev.sergey@gmail.com>
2023-09-16 20:14:18 +0300

    add styles for the form

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {AddButton} from "./components/AddButton/AddButton";
 import {JournalForm} from "./components/JournalForm/JournalForm";
 
 import './App.css'
+
 import {useState} from "react";
 
 
@@ -16,11 +17,13 @@ function App() {
 
     const data = [
         {
+            id: 1,
             title: 'Ну привет!',
             text: 'Здесь можно писать всякое разное пока не надоест.',
             date: new Date()
         },
         {
+            id: 2,
             title: 'Ну привет еще раз, скот!!',
             text: 'Мне уже надоело но я все еще пишу.',
             date: new Date()
@@ -33,9 +36,22 @@ function App() {
         setDataArr(prevState => [...prevState, {
             text: newTask.text,
             title: newTask.title,
-            date: new Date(newTask.date)
+            date: new Date(newTask.date),
+            id: Math.max(...prevState.map(i => i.id)) + 1
         }])
     }
+
+    const sortData = (a, b) => a.date > b.date ? 1 : -1
+
+    const elements = dataArr.sort(sortData).map((el) => (
+        <CardButton key={el.id}>
+            <JournalItem
+                title={el.title}
+                text={el.text}
+                date={el.date}
+            />
+        </CardButton>
+    ))
 
     return (
         <div className='app'>
@@ -43,15 +59,7 @@ function App() {
                 <Header/>
                 <AddButton/>
                 <JournalList>
-                    {dataArr.map((el,i) => (
-                        <CardButton key={i}>
-                            <JournalItem
-                                title={el.title}
-                                text={el.text}
-                                date={el.date}
-                            />
-                        </CardButton>
-                    ))}
+                    {dataArr.length === 0 ? <p>Add new post</p> : elements}
                 </JournalList>
             </LeftSide>
             <MainContent>
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,24 +1,66 @@
-import './JournalForm.css'
+import s from './JournalForm.module.css'
 import {useState} from "react";
 import {Button} from "../Button/Button";
 
+import CalendarIcon from '/Calendar.svg'
+import FolderIcon from '/Folder.svg'
+
 export const JournalForm = ({addNewTask}) => {
-    const [inputValue, setInputValue] = useState('')
+    const [error, setError] = useState(false)
 
     const addJournalForm = (e) => {
         e.preventDefault()
         const formData = new FormData(e.target)
         const formProps = Object.fromEntries(formData)
-        addNewTask(formProps)
+
+        if (formProps.title === '' || !formProps.text) {
+            setError(true)
+        } else {
+            addNewTask(formProps)
+        }
     }
 
     return (
         <>
-            <form className='journal-form' onSubmit={addJournalForm}>
-                <input type="text" name='title'/>
-                <input type="date" name='date'/>
-                <input value={inputValue} type="text" name='tag'/>
-                <textarea name="text" id="" cols="30" rows="10"></textarea>
+            <form className={s.journalForm} onSubmit={addJournalForm}>
+                <div>
+                    <input
+                        type="text"
+                        name='title'
+                        className={s.inputTitle}
+                    />
+                </div>
+                <div className={s.formRow}>
+                    <label htmlFor="date" className={s.formLabel}>
+                        <img src={CalendarIcon} alt="calendar icon"/>
+                        <span>Data</span>
+                    </label>
+                    <input
+                        type="date"
+                        name='date'
+                        id='date'
+                        className={s.input}
+                    />
+                </div>
+                <div className={s.formRow}>
+                    <label htmlFor="tag" className={s.formLabel}>
+                        <img src={FolderIcon} alt="folder icon"/>
+                        <span>Tags</span>
+                    </label>
+                    <input
+                        type="text"
+                        name='tag'
+                        id='tag'
+                        className={s.input}
+                    />
+                </div>
+                <textarea
+                    name="text"
+                    id="" cols="30"
+                    rows="10"
+                    className={s.input}
+                >
+                </textarea>
                 <Button text='Сохранить' />
             </form>
         </>
